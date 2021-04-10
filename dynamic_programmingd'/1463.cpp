2075113b085105a755688ccfb4bab4c7bd8cfb8b
Jieun Han <namu1092@ajou.ac.kr>
2021-04-10 12:17:44 +0900

    dynamic_programming_1463

---
+++ dynamic_programmingd'/1463.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int d[1000001];
+
+int main()
+{
+    scanf("%d", &n);
+
+    for (int i = 2; i <= n; i++)
+    {
+        d[i] = d[i - 1] + 1;
+
+        if (i % 2 == 0)
+        {
+            d[i] = min(d[i], d[i / 2] + 1);
+        }
+        if (i % 3 == 0)
+        {
+            d[i] = min(d[i], d[i / 3] + 1);
+        }
+    }
+
+    printf("%d", d[n]);
+
+    return 0;
+}
