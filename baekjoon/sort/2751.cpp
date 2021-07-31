2d671bc9b7c6d7f166a85d562845fa5115df670e
Jieun Han <namu1092@ajou.ac.kr>
2021-07-31 17:57:17 +0900

    sort_2751

---
+++ baekjoon/sort/2751.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    int array[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    sort(array, array + N);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d\n", array[i]);
+    }
+    return 0;
+}
