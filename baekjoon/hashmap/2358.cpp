102a1bd6b55aaa03d9a053e83cc1b55ebb29df62
Jieun Han <namu1092@ajou.ac.kr>
2021-08-08 10:26:18 +0900

    hashmap_2358

--- baekjoon/hashmap/2358.cpp
+++ baekjoon/hashmap/2358.cpp
@@ -1,14 +1,12 @@
 #include <cstdio>
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
-/* 푸는 중 */
-int n;
-vector<pair<int, int> > coordinates;
+/* 메모리 초과 */
+int n, cnt = 0;
+map<int, int> checkX;
+map<int, int> checkY;
 
 int main()
 {
@@ -18,7 +16,59 @@ int main()
     {
         int x, y;
         scanf("%d %d", &x, &y);
-        coordinates.push_back(make_pair(x, y));
+        if (checkX.find(x) != checkX.end())
+        {
+            checkX.insert(make_pair(x, checkX[x]++));
+        }
+        else
+        {
+            checkX.insert(make_pair(x, 1));
+        }
+
+        if (checkY.find(y) != checkY.end())
+        {
+            checkY.insert(make_pair(y, checkY[y]++));
+        }
+        else
+        {
+            checkY.insert(make_pair(y, 1));
+        }
+        // checkX[x]++;
+        // checkY[y]++;
     }
+
+    map<int, int>::iterator iter;
+    for (iter = checkX.begin(); iter != checkX.end(); iter++)
+    {
+        if ((*iter).second >= 2)
+        {
+            cnt++;
+        }
+    }
+    // for (int i = 0; i < checkX.size(); i++)
+    // {
+    //     if (checkX[i] >= 2)
+    //     {
+    //         cnt++;
+    //     }
+    // }
+
+    for (iter = checkY.begin(); iter != checkY.end(); iter++)
+    {
+        if ((*iter).second >= 2)
+        {
+            cnt++;
+        }
+    }
+    // for (int i = 0; i < checkY.size(); i++)
+    // {
+    //     if (checkY[i] >= 2 && checkX[i] < 2)
+    //     {
+    //         cnt++;
+    //     }
+    // }
+
+    printf("%d", cnt);
+
     return 0;
 }
