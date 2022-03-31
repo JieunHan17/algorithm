7cf4c4ad90df57dd533afd779242ec63a7def788
Jieun Han <hanjieun9874@gmail.com>
2022-03-31 21:38:15 +0900

    backtracking_15664

---
+++ baekjoon/backtracking/15664.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+#define MAX 9
+
+using namespace std;
+
+int N, M;
+vector<int> input;
+int output[MAX];
+bool isUsed[MAX];
+
+void func(int K, int idx)
+{
+    if (K == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            printf("%d ", output[i]);
+        }
+        printf("\n");
+        return;
+    }
+    int prev = 0;
+    for (int i = idx; i < N; i++)
+    {
+        if (!isUsed[i] && prev != input[i])
+        {
+            isUsed[i] = true;
+            output[K] = input[i];
+            prev = input[i];
+            func(K + 1, i);
+            isUsed[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    scanf("%d %d", &N, &M);
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        scanf("%d", &num);
+        input.push_back(num);
+    }
+    sort(input.begin(), input.end());
+
+    func(0, 0);
+    return 0;
+}
