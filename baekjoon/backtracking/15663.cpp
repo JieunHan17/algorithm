604a7bc9ff1ca8c390d4a1bbacee26b0a9910c8f
Jieun Han <hanjieun9874@gmail.com>
2022-03-31 19:28:25 +0900

    backtracking_15663_next_permutation

--- baekjoon/backtracking/15663.cpp
+++ baekjoon/backtracking/15663.cpp
@@ -8,34 +8,8 @@ using namespace std;
 
 int N, M;
 vector<int> input;
-int output[MAX];
-bool isUsed[MAX];
-
-void func(int K)
-{
-    if (K == M)
-    {
-        for (int i = 0; i < M; i++)
-        {
-            printf("%d ", output[i]);
-        }
-        printf("\n");
-        return;
-    }
-
-    int prev = 0;
-    for (int i = 0; i < N; i++)
-    {
-        if (!isUsed[i] && prev != input[i])
-        {
-            isUsed[i] = true;
-            output[K] = input[i];
-            prev = input[i];
-            func(K + 1);
-            isUsed[i] = false;
-        }
-    }
-}
+vector<int> arr;
+vector<vector<int> > output;
 
 int main()
 {
@@ -48,7 +22,44 @@ int main()
     }
     sort(input.begin(), input.end());
 
-    func(0);
+    for (int i = 0; i < M; i++)
+    {
+        arr.push_back(0);
+    }
+    for (int i = M; i < N; i++)
+    {
+        arr.push_back(1);
+    }
+
+    do
+    {
+        vector<int> temp;
+        for (int i = 0; i < N; i++)
+        {
+            if (arr[i] == 0)
+            {
+                temp.push_back(input[i]);
+            }
+        }
+
+        do
+        {
+            output.push_back(temp);
+        } while (next_permutation(temp.begin(), temp.end()));
+        
+    } while (next_permutation(arr.begin(), arr.end()));
+
+    sort(output.begin(), output.end());
+    output.erase(unique(output.begin(), output.end()), output.end());
+
+    for (int i = 0; i < output.size(); i++)
+    {
+        for (int j = 0; j < output[i].size(); j++)
+        {
+            printf("%d ", output[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
